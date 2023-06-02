3d627732788303ae7eb7903eec2821fe9534e967
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-06-01 22:46:52 -0500

    added get,post and render list

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './FamousSection.css';
 
 function FamousSection() {
@@ -7,19 +8,40 @@ function FamousSection() {
   let [famousPeopleArray, setPeopleArray] = useState([]);
 
   // TODO: on load, call the fetchPeople() function
+  useEffect(()=>{
+    fetchPeople();
+  },[])
 
   const fetchPeople = () => {
     // TODO: fetch the list of people from the server
+    axios.get('/people')
+    .then((response)=>{
+      console.log('response data is', response.data)
+      setPeopleArray(response.data)
+    }).catch((error)=>{
+      console.log('there is a error in get function', error)
+    })
   }
-
-  const addPerson = (evt) => {
-    evt.preventDefault();
-    console.log(`The person is ${famousPersonName} and they're famous for ${famousPersonRole}`);
-    
-    // TODO: create POST request to add this new person to the database
+//when do we do response or result
+// TODO: create POST request to add this new person to the database
 
     // HINT: the server is expecting a person object 
     //       with a `name` and a `role` property
+  const addPerson = (evt) => {
+    evt.preventDefault();
+    console.log(`The person is ${famousPersonName} and they're famous for ${famousPersonRole}`);
+    axios.post('/people',{
+      name: famousPersonName,
+      role: famousPersonRole,
+    }).then((response)=>{
+      fetchPeople();
+      setPersonName('')
+      setPersonRole('')
+    }).catch((error)=>{
+      console.log('there is an error in post function', error)
+    })
+    
+    
   
   }
 
@@ -37,6 +59,11 @@ function FamousSection() {
         </p>
         <ul>
           {/* TODO: Render the list of famous people */}
+          {famousPeopleArray.map(people=>{
+            <li key={people.id}>
+              {people.name} is famous for {people.role}
+            </li>
+          })}
         </ul>
       </section>
     );
